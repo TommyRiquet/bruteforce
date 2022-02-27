d7a8446c83a0d73142ba88f3f88647389e590489
Neøw <71312500+TommyRiquet@users.noreply.github.com>
2022-02-28 00:11:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,42 +1,58 @@
 import time
 import keyboard
-from tkinter import *
+import tkinter as tk
 
 valMin = 65
 valMax = 91
 
 
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.delay = 3
+        self.speed = 0
+        self.iteration = 1000000
+        self.isOn = False
+        self.password = ''
+        self.cpt = 0
+
+        self.resizable(False, False)
+        self.geometry("300x300")
+
+        self.Val = tk.StringVar()
+        self.nbrPassword = tk.StringVar()
+
+        tk.Label(self, text='Mot de passe : ').grid(row=2, column=1)
+        tk.Label(self, textvariable=self.Val).grid(row=2, column=4)
+
+        tk.Label(self, text='Numéro : ').grid(row=3, column=1)
+        tk.Label(self, textvariable=self.nbrPassword).grid(row=3, column=4)
+
+        self.btnStart = tk.Button(self, text="Start", font=('Arial', 12), command=self.runBruteForce).grid(row=6, column=3)
+        self.btnStop = tk.Button(self, text="Stop", font=('Arial', 12), command=self.stopBruteForce).grid(row=6, column=4)
 
 
-
-class BruteForce:
-    def __init__(self,test):
-        self.delay = 1
-
-        frame = Frame(window)
-        self.Val = StringVar()
-        labelVal = Label(frame, textvariable=self.Val, font=('Arial', 15), fg='black')
-        labelVal.pack()
-        btnStart = Button(frame, text="Start", font=('Arial', 12), command=self.runBruteForce)
-        btnStart.pack()
-        frame.pack()
-
-    def printVal(self, val):
-        self.Val.set(val)
 
     def runBruteForce(self):
         self.val1 = valMin
         self.val2 = valMin
         self.val3 = valMin
         self.val4 = valMin
+        self.isOn = True
 
-        for i in range(100):
-            window.after(self.delay, self.printVal, chr(self.val1)+chr(self.val2)+chr(self.val3)+chr(self.val4))
-
-            """keyboard.write(chr(val1)+chr(val2)+chr(val3)+chr(val4))"""
+        time.sleep(self.delay)
+        while self.isOn:
+            self.password = chr(self.val1)+chr(self.val2)+chr(self.val3)+chr(self.val4)
+            self.Val.set(self.password)
+            self.nbrPassword.set(self.cpt)
+            app.update()
+            keyboard.write(self.password)
             keyboard.press_and_release('enter')
             keyboard.press_and_release('enter')
-            time.sleep(self.delay)
+            if self.speed != 0:
+                time.sleep(self.speed)
+
+            self.cpt = self.cpt+1
             self.val1 = self.val1+1
             if self.val1 == 91:
                 self.val1 = 97
@@ -53,17 +69,11 @@ class BruteForce:
             if self.val3 == 122:
                 self.val3 = 65
                 self.val4 = self.val4+1
-            if keyboard.is_pressed('esc'):
-                exit(0)
-                window.close()
 
+    def stopBruteForce(self):
+        self.isOn = False
 
 
 if __name__ == '__main__':
-    window = Tk()
-    window.title('BruteForce')
-    window.geometry('300x300')
-    window.resizable(False, False)
-
-    bruteforce = BruteForce(window)
-    window.mainloop()
+    app = App()
+    app.mainloop()
