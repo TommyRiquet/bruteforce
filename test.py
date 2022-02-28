3e3f0f481ff3af48e9a5a3aa2ed72b9f6ebaafae
Neøw <71312500+TommyRiquet@users.noreply.github.com>
2022-02-28 15:37:16 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,19 +5,29 @@ import tkinter as tk
 valMin = 65
 valMax = 91
 
+'''
+Trying to find another way to type the password coz it's too fast
+
+'''
+
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.delay = 3
-        self.speed = 0
-        self.iteration = 1000000
-        self.isOn = False
-        self.password = ''
-        self.cpt = 0
-
         self.resizable(False, False)
         self.geometry("300x300")
+        self.title('BruteForce')
+
+        self.delay = 5
+        self.speed = 0
+        self.isSearching = False
+        self.password = ''
+        self.nbr_essai = 0
+
+        self.val1 = valMin
+        self.val2 = valMin
+        self.val3 = valMin
+        self.val4 = valMin
 
         self.Val = tk.StringVar()
         self.nbrPassword = tk.StringVar()
@@ -31,28 +41,29 @@ class App(tk.Tk):
         self.btnStart = tk.Button(self, text="Start", font=('Arial', 12), command=self.runBruteForce).grid(row=6, column=3)
         self.btnStop = tk.Button(self, text="Stop", font=('Arial', 12), command=self.stopBruteForce).grid(row=6, column=4)
 
-
-
     def runBruteForce(self):
-        self.val1 = valMin
-        self.val2 = valMin
-        self.val3 = valMin
-        self.val4 = valMin
-        self.isOn = True
 
-        time.sleep(self.delay)
-        while self.isOn:
+        for i in range(self.delay, 0, -1):
+            self.nbrPassword.set(i)
+            self.update()
+            time.sleep(1)
+
+        self.isSearching = True
+
+        while self.isSearching:
             self.password = chr(self.val1)+chr(self.val2)+chr(self.val3)+chr(self.val4)
             self.Val.set(self.password)
-            self.nbrPassword.set(self.cpt)
-            app.update()
-            keyboard.write(self.password)
+            self.nbrPassword.set(self.nbr_essai)
+            self.update()
+
+            """keyboard.write(self.password)
             keyboard.press_and_release('enter')
-            keyboard.press_and_release('enter')
+            keyboard.press_and_release('enter')"""
+
             if self.speed != 0:
                 time.sleep(self.speed)
 
-            self.cpt = self.cpt+1
+            self.nbr_essai = self.nbr_essai+1
             self.val1 = self.val1+1
             if self.val1 == 91:
                 self.val1 = 97
@@ -71,7 +82,7 @@ class App(tk.Tk):
                 self.val4 = self.val4+1
 
     def stopBruteForce(self):
-        self.isOn = False
+        self.isSearching = False
 
 
 if __name__ == '__main__':
