7163a0ccf3ea12053c472475df81c796ed27a9d3
Neøw <71312500+TommyRiquet@users.noreply.github.com>
2022-03-01 13:17:38 +0100

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,13 +1,6 @@
 import time
-import keyboard
 import tkinter as tk
-
-valMin = 65
-valMax = 91
-
-'''
-Trying to find another way to type the password coz it's too fast
-'''
+import rarfile
 
 
 class App(tk.Tk):
@@ -16,17 +9,17 @@ class App(tk.Tk):
         self.resizable(False, False)
         self.geometry("300x300")
         self.title('BruteForce')
+        self.filename = 'flag.rar'
 
-        self.delay = 5
         self.speed = 0
         self.isSearching = False
         self.password = ''
         self.nbr_essai = 0
 
-        self.val1 = valMin
-        self.val2 = valMin
-        self.val3 = valMin
-        self.val4 = valMin
+        self.val1 = 48
+        self.val2 = 48
+        self.val3 = 48
+        self.val4 = 48
 
         self.Val = tk.StringVar()
         self.nbrPassword = tk.StringVar()
@@ -37,48 +30,56 @@ class App(tk.Tk):
         tk.Label(self, text='Numéro : ').grid(row=3, column=1)
         tk.Label(self, textvariable=self.nbrPassword).grid(row=3, column=4)
 
-        self.btnStart = tk.Button(self, text="Start", font=('Arial', 12), command=self.runBruteForce).grid(row=6, column=3)
-        self.btnStop = tk.Button(self, text="Stop", font=('Arial', 12), command=self.stopBruteForce).grid(row=6, column=4)
+        self.btnStart = tk.Button(self, text="Start", font=('Arial', 12), command=self.runBruteForce).grid(row=6,
+                                                                                                           column=3)
+        self.btnStop = tk.Button(self, text="Stop", font=('Arial', 12), command=self.stopBruteForce).grid(row=6,
+                                                                                                          column=4)
 
     def runBruteForce(self):
 
-        for i in range(self.delay, 0, -1):
-            self.nbrPassword.set(i)
-            self.update()
-            time.sleep(1)
-
         self.isSearching = True
 
-        while self.isSearching:
-            self.password = chr(self.val1)+chr(self.val2)+chr(self.val3)+chr(self.val4)
-            self.Val.set(self.password)
-            self.nbrPassword.set(self.nbr_essai)
-            self.update()
+        with rarfile.RarFile(self.filename) as rf:
 
-            """keyboard.write(self.password)
-            keyboard.press_and_release('enter')
-            keyboard.press_and_release('enter')"""
+            while self.isSearching:
+                self.password = chr(self.val1) + chr(self.val2) + chr(self.val3) + chr(self.val4)
+                self.Val.set(self.password)
+                self.nbrPassword.set(self.nbr_essai)
+                self.update()
+                try:
+                    rf.extractall(pwd=self.password)
+                    print("Password Found : "+self.password)
+                    self.isSearching = False
+                except rarfile.BadRarFile:
 
-            if self.speed != 0:
-                time.sleep(self.speed)
+                    # [48-57][65-90][97-122]
+                    self.nbr_essai = self.nbr_essai + 1
+                    self.val1 = self.val1 + 1
+                    if self.val1 == 58:
+                        self.val1 = 65
+                    if self.val1 == 91:
+                        self.val1 = 97
+                    if self.val1 == 123:
+                        self.val1 = 65
+                        self.val2 = self.val2 + 1
 
-            self.nbr_essai = self.nbr_essai+1
-            self.val1 = self.val1+1
-            if self.val1 == 91:
-                self.val1 = 97
-            if self.val1 == 122:
-                self.val1 = 65
-                self.val2 = self.val2 + 1
-            if self.val2 == 91:
-                self.val2 = 97
-            if self.val2 == 122:
-                self.val2 = 65
-                self.val3 = self.val3+1
-            if self.val3 == 91:
-                self.val3 = 97
-            if self.val3 == 122:
-                self.val3 = 65
-                self.val4 = self.val4+1
+                    if self.val2 == 58:
+                        self.val2 = 65
+                    if self.val2 == 91:
+                        self.val2 = 97
+                    if self.val2 == 122:
+                        self.val2 = 65
+                        self.val3 = self.val3 + 1
+
+                    if self.val3 == 58:
+                        self.val3 = 65
+                    if self.val3 == 91:
+                        self.val3 = 97
+                    if self.val3 == 122:
+                        self.val3 = 65
+                        self.val4 = self.val4 + 1
+                    if self.val4 == 123:
+                        print('LIMIT')
 
     def stopBruteForce(self):
         self.isSearching = False
